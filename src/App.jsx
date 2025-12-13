cec5de4a5e61882a2f9dc23a31529f0cf90396d7
DIVAKAR <rdivakar0110@gmail.com>
2025-12-13 14:36:37 +0530

    Initial commit - todo frontend

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+// src/App.jsx
+import { useEffect, useState } from "react";
+import SplashScreen from "./Components/SplashScreen";
+import Home from "./Components/Home.jsx";
+import Complaints from "./Components/Complaints.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CitizenSignup from "./Components/Signup/CitizenSignup.jsx";
+import AboutUs from "./Components/About/AboutUs.jsx";
+import ContactUs from "./Components/ContactUs/ContactUs.jsx";
+import CitizenLogin from "./Components/Login/CitizenLogin.jsx";
+
+export default function App() {
+  const [showSplash, setShowSplash] = useState(true);
+
+  useEffect(() => {
+    const t = setTimeout(() => setShowSplash(false), 3000);
+    return () => clearTimeout(t);
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <div className="page-root">
+        {showSplash ? (
+          <SplashScreen />
+        ) : (
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/start_complaints" element={<Complaints />} />
+
+            <Route path="/citizen_signup" element={<CitizenSignup />} />
+            <Route path="/citizen_login" element={<CitizenLogin />} />
+            <Route path="/about_us" element={<AboutUs />} />
+            <Route path="/contact_us" element={<ContactUs />} />
+          </Routes>
+        )}
+      </div>
+    </BrowserRouter>
+  );
+}
